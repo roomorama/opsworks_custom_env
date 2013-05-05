99cb2c1ebf59ba6ce345fc75add3f08c90a43909
Donald Piret <donald@donaldpiret.com>
2013-05-06 02:46:39 +0800

    Add debug code

--- recipes/update.rb
+++ recipes/update.rb
@@ -1,8 +1,11 @@
 # Fast-apply an app's custom configuration.
 
+Chef::Log.debug("Update recipe called in instance with layers #{node[:opsworks][:instance][:layers].inspect}")
+
 if node[:opsworks][:instance][:layers].include?('rails-app')
+  Chef::Log.debug("Node is a rails app server, deploy node: #{node[:deploy].inspect}")
   node[:deploy].each do |application, deploy|
-  
+    Chef::Log.debug("Deploying application #{application}, deploy resource: #{deploy.inspect}")
     # It would be nice to skip when the app's application_type isn't "rails,"
     # but that isn't supplied in the JSON when executing individual recipes.
     
