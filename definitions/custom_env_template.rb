1e1937a7fcbecd75ce7fc3d793cad685fa79b00d
Donald Piret <donald@donaldpiret.com>
2013-05-06 03:48:47 +0800

    Fixes

--- definitions/custom_env_template.rb
+++ definitions/custom_env_template.rb
@@ -7,19 +7,19 @@
 
 define :custom_env_template do
   Chef::Log.debug("Creating custom env template definition with: #{params.inspect}")
-  
-  application = params[:app]
-  deploy = params[:deploy_data]
 
-  template "#{deploy[:deploy_to]}/shared/config/.env.#{deploy[:rails_env]}" do
+  application = params[:application]
+  deploy = params[:deploy]
+
+  template "#{deploy['deploy_to']}/shared/config/.env.#{deploy['rails_env']}" do
     source ".env.erb"
-    owner params[:deploy][:user]
-    group params[:deploy][:group]
+    owner deploy['user']
+    group deploy['group']
     mode "0660"
     variables :env => params[:env]
     notifies :run, resources(:execute => "restart Rails app #{params[:application]}")
 
-    only_if { File.exists?("#{deploy[:deploy_to]}/shared/config") }
+    only_if { File.exists?("#{deploy['deploy_to']}/shared/config") }
   end
 
 end
