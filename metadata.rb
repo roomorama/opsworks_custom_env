f32b480b8f079d2bad8c399aad307603a5d2ac56
Marcos Beirigo <marcos@roomorama.com>
2015-03-27 12:14:29 +0800

    Adds a name and upgrades recipes descriptions

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,10 @@
-maintainer "Artsy"
-description "Writes a config/application.yml file with custom ENV values to apps' deploy directories."
+name "opsworks_custom_env"
+maintainer "Roomorama"
+description "Writes a .env file with custom ENV values to apps' deploy directories."
 version "0.1"
 
-recipe "opsworks_custom_env::configure", "Write a config/application.yml file to app's deploy directory. Relies on restart command declared by rails::configure recipe. (Intended as part of configure/deploy OpsWorks events.)"
-recipe "opsworks_custom_env::update", "Write an updated config/application.yml and restart the app. Can be run independently of OpsWorks configure/deploy events."
+recipe "opsworks_custom_env::configure", "Write a .env file to app's deploy directory. Relies on restart command declared by rails::configure recipe. (Intended as part of configure/deploy OpsWorks events.)"
+recipe "opsworks_custom_env::update", "Write an updated .env and restart the app. Can be run independently of OpsWorks configure/deploy events."
 
 # This actually depends on the rails::configure recipe by OpsWorks, but not
 # declaring that here to prevent librarian-chef failure.
